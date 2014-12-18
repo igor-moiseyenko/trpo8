f3969fdeef40f2496f29de21d9fb63779d17c467
Igor Moiseyenko <imoiseyenko93@gmail.com>
2014-12-18 19:38:33 +0300

    SPA Structure project renaming to the TRPO Lab 8.

---
+++ application/frontend/trpo8-web-client/src/main/webapp/resources/js/app/repository/account/accountRepository.js
@@ -0,0 +1,32 @@
+/**
+ * Account repository module.
+ * 
+ * @author imoiseyenko93@gmail.com
+ */
+define([ "angular", "angular-resource" ], function(angular, ngResource) {
+
+	var accountRepositoryModule = angular.module("accountRepositoryModule",
+			[ "ngResource" ]);
+
+	accountRepositoryModule.factory("accountRepository", [
+			"$resource",
+			function($resource) {
+
+				var apiHost = "http://localhost:8080/trpo8-api";
+
+				return {
+
+					findAccountByUsername : function(username) {
+
+						var accountResource = $resource(apiHost
+								+ "/api/v1/accounts/:username");
+
+						return accountResource.get({
+							username : username
+						});
+					}
+				};
+			} ]);
+
+	return accountRepositoryModule;
+});
